b5cf9f5417b44982f6c12da5d559ee3255ddaa22
Layth Al-Qattan <laythq@gmail.com>
2018-09-09 12:10:38 +0100

    Imported App module from separate .jsx file, modified webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,10 @@ module.exports = {
     {
       test: /\.js$/,
       exclude: /node_modules/,
+      loader: "babel-loader" },
+    {
+      test: /\.jsx$/,
+      exclude: /node_modules/,
       loader: "babel-loader" }
     ]
   }
