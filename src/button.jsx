9a575d3019a8f3a1b90c62bdfb5075de1d2db384
Layth Al-Qattan <laythq@gmail.com>
2018-09-09 19:44:32 +0100

    Button styling

---
+++ src/button.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import css from './button.css'
+
+export default class Button extends React.Component {
+  render() {
+    return (
+      <div>
+      <button />
+      <button />
+      </div>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx'
+import Button from './button.jsx'
 
 ReactDOM.render(
-  <App />,
-  document.getElementById("root")
+  <Button />,
+  document.getElementById("capacity")
 );
+ 
