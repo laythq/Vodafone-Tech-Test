660ba6a728b4bd988cd9caffbeaf8340d45734ad
Layth <laythq@gmail.com>
2018-09-10 17:50:18 +0100

    Update picture component responsiveness

--- src/colour.jsx
+++ src/colour.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import './colour.css'
+import {updateImage} from './picture.jsx'
+
+const goldImage = "./Apple_iPhone_8_Gold-full-product-front.png"
+const silverImage = "./Apple_iPhone_8_Silver_WS2-full-product-front.png"
+const greyImage = "./Apple_iPhone_8_Space_Grey_WS2-full-product-front.png"
 
 export default class Colour extends React.Component {
   constructor(props){
@@ -10,19 +15,20 @@ export default class Colour extends React.Component {
     this.handleClick = this.handleClick.bind(this)
   }
 
-  handleClick(new_colour) {
+  handleClick(new_colour, image) {
       this.setState(state => ({
         colour: new_colour
       }));
+      updateImage(image)
     }
 
   render() {
     return (
       <div>
         <p>Colour: {this.state.colour}</p>
-        <button id="gold" onClick={this.handleClick.bind(this, 'Gold')}></button>
-        <button id="silver" onClick={this.handleClick.bind(this, 'Silver')}></button>
-        <button id="space_grey" onClick={this.handleClick.bind(this, 'Space Grey')}></button>
+        <button id="gold" onClick={this.handleClick.bind(this, 'Gold', goldImage)}></button>
+        <button id="silver" onClick={this.handleClick.bind(this, 'Silver', silverImage)}></button>
+        <button id="space_grey" onClick={this.handleClick.bind(this, 'Space Grey', greyImage)}></button>
       </div>
     )
   }
--- src/picture.jsx
+++ src/picture.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 
 export default class Picture extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      image: "./Apple_iPhone_8_Gold-full-product-front.png"
+    }
+    updateImage = updateImage.bind(this)
+  }
+
   render() {
     return (
       <div>
-      <img src="./Apple_iPhone_8_Gold-full-product-front.png" class="img-fluid" alt="Responsive image"></img>
+      <img src={this.state.image} class="img-fluid" alt="Responsive image"></img>
       </div>
     )
   }
+}
+
+export function updateImage(new_image){
+  this.setState(state => ({
+    image: new_image
+  }))
 }
