8dc88e0c026d944e9690a5e6b3eb08c6ac6572c5
Layth <laythq@gmail.com>
2018-09-10 21:19:57 +0100

    Adjust button margins

--- src/capacity.jsx
+++ src/capacity.jsx
@@ -23,8 +23,8 @@ export default class Capacity extends React.Component {
     return (
       <div id="buttons">
       <p>Capacity: {this.state.capacity}</p>
-      <button onClick={this.handleClick.bind(this, '64', '£43.20', '£149')}>64</button>
-      <button onClick={this.handleClick.bind(this, '256', '£46.80', '£275')}>256</button>
+      <button id="sixtyfour" onClick={this.handleClick.bind(this, '64', '£43.20', '£149')}>64</button>
+      <button id="twofivesix" onClick={this.handleClick.bind(this, '256', '£46.80', '£275')}>256</button>
       </div>
     )
   }
