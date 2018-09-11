00f945ce7a0583dbfb4c061aea63a4f72a935b0a
Layth <laythq@gmail.com>
2018-09-11 15:59:32 +0100

    Added monthlyPrice test

--- src/monthlyPrice.test.jsx
+++ src/monthlyPrice.test.jsx
@@ -7,4 +7,9 @@ describe('Monthly Price Component', () => {
     const wrapper = shallow(<MonthlyPrice />)
     expect(wrapper.find('p').text()).toEqual('When you pay £43.20 a month')
   })
+  it('should update the price description when state is changed', () => {
+    const wrapper = shallow(<MonthlyPrice />)
+    wrapper.setState({price: '£100'})
+    expect(wrapper.find('p').text()).toEqual('When you pay £100 a month')
+  })
 })
